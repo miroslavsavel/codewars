fedd7950402f1b4ba7d97f2fbf6a91d45742dbfe
Miroslav Savel <miroslav.savel@gmail.com>
2022-08-11 15:22:49 +0200

    greedy knapsack solution

--- knapsack/knapsack_greedy.py
+++ knapsack/knapsack_greedy.py
@@ -11,8 +11,21 @@ def greedy_knapsack(items, capacity):
     # find the fruit with the highest profit
     # first we sort the list by key for value using lambda
     items = sorted(items,key=lambda item:item[1], reverse=True)     #??check for lambdas
-    print(items)
+    # print(items)
+    chosen_fruits = {}
+    profit = 0
+    for i in range(len(items)):
+        # for each fruit in sorted array
+        # 1. we want know how much of this fruit shoudl can we add to the bag, how many times the fruit can we put in the bag
+        # 2. and how much capacity left
+        name, value, weight = items[i]    #tupple unpacking
+        # how many fruits of this type can we take
+        num_of_fruit = (capacity - capacity % weight) / weight
+        chosen_fruits[name] = num_of_fruit
+        capacity = capacity % weight
+        profit += num_of_fruit * value
+    return round(profit,2), chosen_fruits
 
 
-# test greedy
-greedy_knapsack(items, 2000)
+# test greedy ! this ist optimal solution
+print(greedy_knapsack(items, 2000))
